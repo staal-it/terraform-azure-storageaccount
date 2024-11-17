c08435109be8a515e7ccd2d679147b12ea0b78b6
Erwin Staal <erwin.staal@klm.com>
2024-11-17 20:21:57 +0100

    Add initial Terraform configuration for Azure storage account

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "storage_account_name" {
+  description = "The name of the storage account"
+  type        = string  
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the storage account"
+  type        = string
+}
