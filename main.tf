fbf3a3ae589a841ae3fb4b7d6e7951932d15d0ed
Erwin Staal <erwin.staal@xebia.com>
2024-12-11 14:58:38 +0100

    Refactor storage account resource to use variables for resource group name and location

--- main.tf
+++ main.tf
@@ -11,14 +11,10 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
-}
-
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = data.azurerm_resource_group.rg.location
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,15 @@
 variable "storage_account_name" {
   description = "The name of the storage account"
-  type        = string  
+  type        = string
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group in which to create the storage account"
   type        = string
+}
+
+variable "location" {
+  description = "The location/region where the storage account should be created"
+  type        = string
+  default     = "westeurope"
 }
